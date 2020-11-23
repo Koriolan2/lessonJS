22c5da54f778a64fb24fdf0231d530c9669429d9
Kozmin Jurii <Koriolan2@gmail.com>
2020-11-24 00:23:59 +0200

    Второе задание

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit;
-let mission; 
-let period;
+let money = 12300;
+let income = 20000; 
+let addExpenses = 'Интернет, Такси, Коммунальные'; 
+let deposit = false;
+let mission = 1000000; 
+let period = 10;
 
-alert ('Сообщение в модальном окне!');
+console.log('money: ', money);
+console.log('income: ', income);
+console.log('deposit: ', deposit);
 
-console.log('Сообщение в консоли');
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' евро');
+
+let newAddExpenses = addExpenses.toLowerCase();
+
+console.log(newAddExpenses.split(', '));
+
+
